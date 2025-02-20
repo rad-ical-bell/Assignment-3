e81f40e207fc84c65a4e78c4d65f612b967715b0
rad-ical-bell <westiequeenrad@gmail.com>
2025-02-20 10:48:01 -0500

    Particles, show/hide

--- Assignment 3/Assets/Movement.cs
+++ Assignment 3/Assets/Movement.cs
@@ -24,8 +24,26 @@ public class Movement : MonoBehaviour
         }
         else
         {
+            Spin();
+        }
+    }
+
+    void Spin()
+    {
+        if (!rotateComplete)
+        {
             Quaternion targetRotation = Quaternion.Euler(-90f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * speed * 100f);
+            if(transform.rotation.x == -90f)
+            {
+                rotateComplete = true;
+            }
         }
+        else
+        {
+            Quaternion targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, -360f);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * speed * 100f);
+        }
+        
     }
 }
